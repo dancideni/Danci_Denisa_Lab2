14609d46d959c8c491f21f36be03e2ecaf92ba71
Denisa Danci <denisadanci00@gmail.com>
2021-10-31 22:55:34 +0200

    Laborator 4

---
+++ StopCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Danci_Denisa_Lab2.CustomCommands
+{
+    class StopCommand
+    {
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Launch", "Launch", typeof(StopCommand), myInputGestures);
+        }
+        public static RoutedUICommand Launch { get; private set; }
+    }
+}
